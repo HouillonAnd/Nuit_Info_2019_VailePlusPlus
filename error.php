1f56662fa53c7040337aa0f8f9d99aa9cefabcd4
HouillonAnd <43677104+HouillonAnd@users.noreply.github.com>
2019-12-05 19:30:21 +0100

    Create error.php
    To Do : Afficher images

---
+++ error.php
@@ -0,0 +1,39 @@
+<?php
+$bug = 'controller';
+	switch ($bug) {
+		case 'controller':
+			echo"http://fr.hellokids.com/r_2620/coloriage/coloriage-metier/coloriage-metier-sncf"
+			echo "Ce controller n'exite pas, désolé.";
+			break;
+
+		case 'fonction':
+			echo"http://tanopah.jo.free.fr/seconde/Fdef.html"
+			echo "Cette fonction n'exite pas, désolé.";
+			break;
+
+		case 'login'
+			echo"https://www.deviantart.com/irkenartwork12/art/Behold-Meme-template-533892657"
+			echo "Le coupe utilisateur - Login choisi n'existe pas, veuillez vérifier votre identifiant et votre mot de passe.";
+			break;
+
+		case 'notExists'
+			echo"https://www.datanumen.com/blogs/4-approaches-outlook-error-file-not-exist/"
+			echo "Ce que vous cherchez n'existe pas. Veuillez rechercher autre chose";
+			break;
+
+		case 'internalError'
+			echo"https://imgflip.com/memetemplate/20825644/picard-oops"
+			echo "Désolé, il serait possible que l'on ait mal codé.";
+			break;
+
+		case 'notAuthorized'
+			echo"https://knowyourmeme.com/memes/jack-blacks-octagon"
+			echo "Vous ne possédez pas les droits nécessaires pour aller ici.";
+			break;
+
+		default:
+			echo "Comment êtes-vous arrivés là?";
+			echo "source : <a>http://www.quickmeme.com/meme/3pgqjq</a>";
+			break;
+	}
+?>
