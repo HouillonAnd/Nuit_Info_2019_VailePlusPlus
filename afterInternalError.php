728e86052ccaaf4c384703fc8a6f0a0439b9b513
TheoManelphe <43880934+TheoManelphe@users.noreply.github.com>
2019-12-06 05:25:17 +0100

    j'ai tout changé

--- afterInternalError.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo '<link rel="stylesheet" href="https://unpkg.com/@clr/ui/clr-ui.min.css" />'
-
-		echo "<img src='src/star' alt='Desc'>"
-			// source https://i3.kym-cdn.com/entries/icons/original/000/001/569/insp_captkirk_5_.jpg"
-		
-?>
